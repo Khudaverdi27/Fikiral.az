99aa92a720600bc1de23ec910ae9ef74d2b11f0e
Xudaverdi <xudaverdi.sh@gmail.com>
2024-02-24 16:25:41 +0400

    fixed:disable like and dislike for unregistered users

--- src/components/widget/Thinks/ThinkCardActions.jsx
+++ src/components/widget/Thinks/ThinkCardActions.jsx
@@ -4,7 +4,7 @@ import { VscLink } from "react-icons/vsc";
 import AddCommentModal from "../../ui/Modals/AddCommentModal";
 import { useState } from "react";
 import { usePutLikeAndDislike } from "../../../hooks/useFetch";
-import { getStorage, removeStorage, saveStorage } from "../../../utils/helpers";
+import { getStorage } from "../../../utils/helpers";
 function ThinkCardActions({
   disabled = true,
   allComments,
@@ -22,7 +22,7 @@ function ThinkCardActions({
   const [dislike, setDislike] = useState(false);
   const [count, setCount] = useState(likes);
   const [fetchLikeCount, loading] = usePutLikeAndDislike();
-
+  const token = getStorage("token");
   const likeActions = () => {
     if (!like) {
       const updatedCount = dislike ? count + 2 : count + 1;
@@ -53,19 +53,27 @@ function ThinkCardActions({
   return (
     <div className="flex items-center justify-between ">
       <div className="flex items-center gap-x-[17px] max-h-6 group">
-        <button onClick={dislikeActions}>
+        <button
+          disabled={token.length == 0}
+          onClick={dislikeActions}
+          className="disabled:opacity-30 disabled:cursor-not-allowed"
+        >
           <AiOutlineClose
             className={` ${
               like ? "size-[22px] " : "size-7 hover:ml-[0px]"
-            } group-hover:opacity-60  hover:!opacity-100 hover:ml-[-6px]   text-[#292D32] hover:text-black cursor-pointer hover:size-7`}
+            } group-hover:opacity-60  hover:!opacity-100 hover:ml-[-6px]   text-[#292D32] hover:text-black hover:size-7`}
           />
         </button>
         <span className="text-sm font-[500] ">{count}</span>
-        <button onClick={likeActions}>
+        <button
+          disabled={token.length == 0}
+          onClick={likeActions}
+          className="disabled:opacity-30 disabled:cursor-not-allowed"
+        >
           <BsFillHeartFill
             className={` ${
               like ? "size-7 " : "size-[22px] "
-            } group-hover:opacity-60 hover:!opacity-100 text-[#FF0000]   cursor-pointer hover:size-7`}
+            } group-hover:opacity-60 hover:!opacity-100 text-[#FF0000]   hover:size-7`}
           />
         </button>
       </div>
