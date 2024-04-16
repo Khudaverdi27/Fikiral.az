918747154c9478c6028728fd2eb324bcb279c4f1
Xudaverdi <xudaverdi.sh@gmail.com>
2024-04-16 19:59:18 +0400

    fixed:onclose modal reset fields

--- src/components/ui/Form/FormContainer.jsx
+++ src/components/ui/Form/FormContainer.jsx
@@ -14,6 +14,7 @@ const FormContainer = ({ children }) => {
     setMainModel,
     isSubModel,
     onSubModel,
+    reset,
   } = useModalActions();
   const isMobile = useMediaQuery("only screen and (max-width : 480px)");
   return (
@@ -53,7 +54,10 @@ const FormContainer = ({ children }) => {
         className="bg-[#FBFBFB] rounded-[20px]"
         open={isMainModel}
         onOk={() => setMainModel(false)}
-        onCancel={() => setMainModel(false)}
+        onCancel={() => {
+          setMainModel(false);
+          reset();
+        }}
       >
         <div className="flex justify-center items-center pt-7">
           <div className="w-[300px] space-y-3">{children}</div>
@@ -67,7 +71,10 @@ const FormContainer = ({ children }) => {
         className="bg-[#FBFBFB] rounded-[20px]"
         open={isSubModel}
         onOk={(e) => onSubModel(e, false)}
-        onCancel={(e) => onSubModel(e, false)}
+        onCancel={(e) => {
+          onSubModel(e, false);
+          reset();
+        }}
       >
         <div className="flex justify-center items-center pt-10">
           <div
